2eff226fab0d656fe52fc3a2a51eaacd5f45eba8
HyPeRacvtive <mehmetgon@hotmail.com>
2022-02-13 07:05:26 +0300

    Dashboard istatistik verileri çekildi

--- Web.UI/Controllers/AdminController.cs
+++ Web.UI/Controllers/AdminController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Web.UI.Controllers
 {
     //[AllowAnonymous]
     public class AdminController : Controller
     {
+        DataContext context = new DataContext();
         public IActionResult Index()
         {
+
+            ViewBag.ProductCount = context.Products.Count().ToString();
+            ViewBag.SliderCount = context.Sliders.Count().ToString();
+            ViewBag.VideoCount = context.Videos.Count().ToString();
+            ViewBag.MessageCount = context.Messages.Count().ToString();
             return View();
         }
         /*ÜRÜNLER*/
