f3d6a56ff47fff4c2e11c62aafb002295bc4136f
Mehmet GÖN <mehmetgon@hotmail.com>
2022-02-14 14:13:35 +0300

    Havadurumu verileri çekildi

--- Web.UI/ViewComponents/Admin/AdminProfile.cs
+++ Web.UI/ViewComponents/Admin/AdminProfile.cs
@@ -1,15 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete;
 using DataAccess.Concrete.EFCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Web.UI.ViewComponents.Admin
 {
     public class AdminProfile : ViewComponent
     {
         AdminManager am = new AdminManager(new EFAdminRepository());
-        public IViewComponentResult Invoke()
+        DataContext context = new DataContext();
+        public IViewComponentResult Invoke(int id)
         {
-
+            ViewBag.AdminName = context.Admins.Where(x => x.Id == 1).Select(y => y.NameSurname).FirstOrDefault();
+            ViewBag.AdminImage = context.Admins.Where(x => x.Id == 1).Select(y => y.Image).FirstOrDefault();
             return View();
         }
     }
---
+++ Web.UI/ViewComponents/Admin/Weather.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Web.UI.ViewComponents.Admin
+{
+    public class Weather : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            string api = "5c4dc92acf3a2e1cb22a7542b3fc2e03";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=adana&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument doc = XDocument.Load(connection);
+            ViewBag.Temperature = doc.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            ViewBag.City = doc.Descendants("city").ElementAt(0).Attribute("name").Value;
+            ViewBag.Cloud = doc.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+            ViewBag.icon = doc.Descendants("weather").ElementAt(0).Attribute("icon").Value;
+            return View();
+        }
+    }
+}
